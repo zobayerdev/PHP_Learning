2a0304a7beae8ed265cee131a1e0f304522fd96c
zobayerdev <zobayer.dev@gmail.com>
2023-12-27 12:41:15 +0600

    first class code uploaded

---
+++ class_one.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+    <?php
+
+    echo "<h1>Zobayer Hasan Nayem</h1>";
+    $a = 10;
+
+    echo "this inputed number:- $a";
+    var_dump($a);
+
+    // variable define
+    $name = "Nayem";
+    var_dump($name);
+
+    ?>
+
+</body>
+
+</html>
